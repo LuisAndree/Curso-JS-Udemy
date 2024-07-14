903a3b155a41c2da716011dc846d57bf50fca563
Luís André <andre2016999@gmail.com>
2024-07-14 09:19:52 -0300

    filter + map + reduce

---
+++ aulas/secao5/filterMapReduce.js
@@ -0,0 +1,10 @@
+// filtrar pares 
+// dobrar os valores
+// reduzir (somar todos)
+const nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+
+const resultado = nums.filter(num => num % 2 == 0)
+    .map(num => num * 2)
+    .reduce((acumulador, num) => acumulador += num)
+
+console.log(resultado)
